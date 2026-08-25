981d2fb094d89b62ee1da065465c75a34cefd6f3
leo <leno3z4rrrri@gmail.com>
2026-08-26 00:48:38 +0100

    Use Arc testnet RPC in frontend configuration

--- frontend/config/wagmi.js
+++ frontend/config/wagmi.js
@@ -4,24 +4,12 @@ import { defineChain } from 'viem';
 export const arcTestnet = defineChain({
   id: 5042002,
   name: 'Arc Testnet',
-  nativeCurrency: {
-    name: 'USD Coin',
-    symbol: 'USDC',
-    decimals: 6,
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://5042002.rpc.thirdweb.com'],
-    },
-  },
-  blockExplorers: {
-    default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' },
-  },
+  nativeCurrency: { name: 'USD Coin', symbol: 'USDC', decimals: 6 },
+  rpcUrls: { default: { http: ['https://rpc.testnet.arc.network'] } },
+  blockExplorers: { default: { name: 'ArcScan', url: 'https://testnet.arcscan.app' } },
 });
 
 export const config = createConfig({
   chains: [arcTestnet],
-  transports: {
-    [arcTestnet.id]: http(),
-  },
+  transports: { [arcTestnet.id]: http() },
 });
