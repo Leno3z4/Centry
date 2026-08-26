cd36342cd9ff8f7340e103526458b5e62adef99a
leo <leno3z4rrrri@gmail.com>
2026-08-26 21:28:16 +0100

    Format fixed-supply CENT token for readability

--- contracts/token/CentryToken.sol
+++ contracts/token/CentryToken.sol
@@ -4,10 +4,26 @@ pragma solidity ^0.8.24;
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v5.4.0/contracts/token/ERC20/ERC20.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v5.4.0/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-/// @notice Fixed-supply CENT token. No post-deployment mint authority exists.
+/// @title Centry Token
+/// @notice Fixed-supply CENT token with ERC-2612 permit support.
+/// @dev There is intentionally no post-deployment mint function.
 contract CentryToken is ERC20, ERC20Permit {
-    constructor(address initialRecipient, uint256 initialSupply) ERC20("Centry", "CENT") ERC20Permit("Centry") {
-        require(initialRecipient != address(0), "ZERO_RECIPIENT");
-        _mint(initialRecipient, initialSupply);
+    error InvalidRecipient();
+
+    constructor(
+        address initialRecipient,
+        uint256 initialSupply
+    )
+        ERC20("Centry", "CENT")
+        ERC20Permit("Centry")
+    {
+        if (initialRecipient == address(0)) {
+            revert InvalidRecipient();
+        }
+
+        _mint(
+            initialRecipient,
+            initialSupply
+        );
     }
 }
