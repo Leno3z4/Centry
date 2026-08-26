5a072fb03c01f6d2633561e3881a2ab6f09ed101
leo <leno3z4rrrri@gmail.com>
2026-08-26 21:27:56 +0100

    Format oracle interface for readability

--- contracts/interfaces/ICentryOracle.sol
+++ contracts/interfaces/ICentryOracle.sol
@@ -2,5 +2,10 @@
 pragma solidity ^0.8.24;
 
 interface ICentryOracle {
-    function getPrice(address asset) external view returns (uint256 priceE18, uint256 updatedAt);
+    function getPrice(
+        address asset
+    ) external view returns (
+        uint256 priceE18,
+        uint256 updatedAt
+    );
 }
