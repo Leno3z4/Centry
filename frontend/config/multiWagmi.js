b2eab4064defe2ba7709db4481b6a8fcd6a08c56
leo <leno3z4rrrri@gmail.com>
2026-08-27 14:27:43 +0100

    Add multi-wallet discovered Wagmi configuration

---
+++ frontend/config/multiWagmi.js
@@ -0,0 +1,33 @@
+import { createConfig, http } from 'wagmi';
+import { defineChain } from 'viem';
+
+export const arcTestnet = defineChain({
+  id: 5042002,
+  name: 'Arc Testnet',
+  nativeCurrency: {
+    name: 'USD Coin',
+    symbol: 'USDC',
+    decimals: 6,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.testnet.arc.network'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'ArcScan',
+      url: 'https://testnet.arcscan.app',
+    },
+  },
+});
+
+export const config = createConfig({
+  chains: [arcTestnet],
+  connectors: [],
+  transports: {
+    [arcTestnet.id]: http(),
+  },
+  multiInjectedProviderDiscovery: true,
+  ssr: true,
+});
