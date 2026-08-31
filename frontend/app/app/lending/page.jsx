06663f6b5e3d0316a3f41d1513a0b0831cbb44da
leo <leno3z4rrrri@gmail.com>
2026-08-31 14:31:01 +0100

    Create dedicated Lending page

---
+++ frontend/app/app/lending/page.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { Providers } from '../../../components/Providers';
+import { AppShell } from '../../../components/AppShell';
+import MultiMarketLending from '../../../components/MultiMarketLending';
+
+export default function Page() {
+  return <Providers><AppShell><MultiMarketLending /></AppShell></Providers>;
+}
