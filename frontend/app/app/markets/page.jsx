8eb53227b9fac12d18e56861a1ac63b4284bbb8d
leo <leno3z4rrrri@gmail.com>
2026-09-06 17:52:11 +0100

    Make Markets page a market directory

--- frontend/app/app/markets/page.jsx
+++ frontend/app/app/markets/page.jsx
@@ -2,8 +2,8 @@
 
 import { Providers } from '../../../components/Providers';
 import { AppShell } from '../../../components/AppShell';
-import MultiMarketLending from '../../../components/MultiMarketLending';
+import MarketDirectory from '../../../components/MarketDirectory';
 
 export default function MarketsPage() {
-  return <Providers><AppShell><MultiMarketLending /></AppShell></Providers>;
+  return <Providers><AppShell><MarketDirectory /></AppShell></Providers>;
 }
