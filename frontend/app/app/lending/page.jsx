781bb22a123fedf924e629332bea915af8c26c01
leo <leno3z4rrrri@gmail.com>
2026-09-06 18:20:56 +0100

    Redirect legacy lending route to Markets

--- frontend/app/app/lending/page.jsx
+++ frontend/app/app/lending/page.jsx
@@ -1,9 +1,5 @@
-'use client';
-
-import { Providers } from '../../../components/Providers';
-import { AppShell } from '../../../components/AppShell';
-import MultiMarketLending from '../../../components/MultiMarketLending';
+import { redirect } from 'next/navigation';
 
 export default function Page() {
-  return <Providers><AppShell><MultiMarketLending /></AppShell></Providers>;
+  redirect('/app/markets');
 }
