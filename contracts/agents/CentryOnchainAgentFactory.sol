8e6baa381d9224ac4c7c016aa46d7f0dabf4dd02
leo <leno3z4rrrri@gmail.com>
2026-09-17 16:20:08 +0100

    fix: use local OpenZeppelin remapping for agent factory

--- contracts/agents/CentryOnchainAgentFactory.sol
+++ contracts/agents/CentryOnchainAgentFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v5.4.0/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./CentryOnchainAgentAccount.sol";
 
 /// @title Centry Onchain Agent Factory
